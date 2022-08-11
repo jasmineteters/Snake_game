7bab4a9cdb2690d83f92559ffeecfde3c6b86a6c
jasmineteters <jasmineteters2020@gmail.com>
2022-08-10 20:55:13 -0700

    added tail addition functionality and refactored game is over

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
@@ -15,7 +14,6 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -24,21 +22,27 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
-
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-
-#Detect collision with food
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
-# Detect collision with wall
+    # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.game_over()
+    # Detect collision with Tail
+    for segment in snake.segments[1:]:
+        # If head collides with any segment in the tail:
+        if snake.head.distance(segment) < 10:
+            # Trigger game_over
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
