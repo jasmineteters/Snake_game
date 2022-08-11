6c425b12145329624e19df01c40a1d54838ed55f
jasmineteters <jasmineteters2020@gmail.com>
2022-08-10 20:36:12 -0700

    Added classes for food and for scoreboard functionality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -10,6 +12,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -24,8 +28,17 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+
+#Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+# Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
