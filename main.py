9819f4556931dcff5b25200d2b349272c99cfaf6
jasmineteters <jasmineteters2020@gmail.com>
2022-08-16 14:32:35 -0700

    reconfigured scoreboard to track a high score and to continue playing after a loss

--- main.py
+++ main.py
@@ -34,15 +34,18 @@ while game_is_on:
         scoreboard.increase_score()
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
     # Detect collision with Tail
     for segment in snake.segments[1:]:
         # If head collides with any segment in the tail:
-        if snake.head.distance(segment) < 10:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
             # Trigger game_over
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
